be77ef67010242cc3229097212a78fdaeb058e31
adammarshallNR <111354481+adammarshallNR@users.noreply.github.com>
2026-04-09 01:45:24 +0100

    Update global-setup.ts

--- global-setup.ts
+++ global-setup.ts
@@ -1,9 +1,9 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { Resource } from '@opentelemetry/resources';
+import { resourceFromAttributes } from '@opentelemetry/resources';
 
 const sdk = new NodeSDK({
-  resource: new Resource({
+  resource: resourceFromAttributes({
     'service.name': process.env.SERVICE_NAME || 'playwright-e2e',
     'ci.pipeline': process.env.GITHUB_WORKFLOW,
     'ci.job.id': process.env.GITHUB_RUN_ID,
